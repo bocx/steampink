0d0318eaed93b604758d11413e4a7e36f58fda00
Timo Ahtikari <timo.ahtikari@metropolia.fi>
2013-10-10 13:16:56 +0300

    exploding cannonball
    added collision behavior to cannonball and a placeholder particle effect
    for the explosion.

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -5,5 +5,18 @@ using System.Collections;
  * Used for instantiation in Shoot.cs
  **/
 public class CannonBall : MonoBehaviour {
+	
+	public Transform explosionPrefab;
+	
+	void OnCollisionEnter(Collision collision) {
+		ContactPoint contact = collision.contacts[0];
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		Vector3 pos = contact.point;		
 
+		Instantiate(explosionPrefab, pos, rot);
+		
+		Destroy(gameObject);
+	}
+			
 }
+			
