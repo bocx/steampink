25c71d251b870094a14e78ee9581bb8d8a3154ea
Timo Ahtikari <timo.ahtikari@metropolia.fi>
2013-10-13 18:19:48 +0300

    asd

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -8,19 +8,27 @@ public class CannonBall : MonoBehaviour {
 	
 	public Transform explosionPrefab;
 	
-	public float timeToLive = 1f;
+	public float timeToLive, safetyTimer;
+	
+	private bool primed = false;
+	private float timer = 0;
 	
 	void OnCollisionEnter(Collision coll) {
-		
-		coll.gameObject.SendMessage("ApplyDamage", 20, SendMessageOptions.DontRequireReceiver);	
-		
-		ContactPoint contact = coll.contacts[0];
-		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-		Vector3 pos = contact.point;		
-		Instantiate(explosionPrefab, pos, rot);
-		Destroy(gameObject);
+		if (primed){
+			coll.gameObject.SendMessage("ApplyDamage", 20, SendMessageOptions.DontRequireReceiver);	
+			
+			ContactPoint contact = coll.contacts[0];
+			Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+			Vector3 pos = contact.point;		
+			Instantiate(explosionPrefab, pos, rot);
+			Destroy(gameObject);
+		}
 	}
 	void FixedUpdate(){
+		timer += Time.deltaTime;
+		if(timer > safetyTimer){
+			primed = true;
+		}
 		timeToLive -= Time.deltaTime;
 		if (timeToLive <= 0){			
 			Instantiate(explosionPrefab, transform.position, transform.localRotation);
