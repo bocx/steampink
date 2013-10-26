a0e43fb8e1ff7cd9b8f9f309ee8b51e144019c94
Timo Ahtikari <timo.ahtikari@metropolia.fi>
2013-10-26 18:27:05 +0300

    added landingpad for choosing ships

---
+++ Assets/Scripts/LandingPad.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandingPad : MonoBehaviour {
+
+	public Airship prototypeAirship;
+	public Airplane prototypeAirplane;
+	public Jetpack prototypeJetpack;
+	
+	private bool toggleGUI = false;
+	
+	void OnTriggerEnter(Collider col) {
+		toggleGUI = true;
+    }
+	
+	void OnTriggerExit(Collider col) {
+		toggleGUI = false;
+    }
+		
+	
+	void OnGUI () {
+		if(toggleGUI){
+			GUI.Box(new Rect(10,10,100,120), "Choose Ship");
+	
+			if(GUI.Button(new Rect(20,40,80,20), "Airship")) {
+				SelectShip(1);
+			}
+	
+			if(GUI.Button(new Rect(20,70,80,20), "Airplane")) {
+				SelectShip(2);
+			}
+			
+			if(GUI.Button(new Rect(20,100,80,20), "Jetpack")) {
+				SelectShip(3);
+			}
+		}
+	}
+	
+	
+	void SelectShip(int Shiptype){
+		
+		switch (Shiptype){
+    		case 1:
+	        	Debug.Log("Spawn Airship as Player");	
+				Airship PlayerAirship = (Airship)Instantiate(prototypeAirship, transform.position + transform.forward*15f, Quaternion.identity);
+	        	break;
+			case 2:
+		    	Debug.Log("Spawn Airplane as Player");
+				Airplane PlayerAirplane = (Airplane)Instantiate(prototypeAirplane, transform.position + transform.forward*15f, Quaternion.identity);
+	        	break;
+			case 3:
+		    	Debug.Log("Spawn Jetpack as Player");
+				Jetpack PlayerJetpack = (Jetpack)Instantiate(prototypeJetpack , transform.position + transform.forward*15f, Quaternion.identity);
+	        	break;
+			default:
+		    	Debug.Log("Should never happen.");
+		    	break;
+		}	
+	}
+}
